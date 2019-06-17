f0342ac4101143988ecc56a824e9c92fba409ab8
Gabe Wyatt <gwyattkelsey@gmail.com>
2019-06-16 19:57:16 -0700

    Updated docker composition

--- src/db.ts
+++ src/db.ts
@@ -1,13 +1,5 @@
 import mongoose from 'mongoose';
 
-// const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${
-//   process.env.DB_HOST
-// }/${process.env.DB_DATABASE}${process.env.DB_QUERY}`;
-
-// const url = `mongodb://root:example@localhost:27017/api-ping-pub-dev`;
-
-// const url = `mongodb://newuser:newpassword@localhost:27017/testdb`;
-
 const url = `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${
   process.env.DB_HOST
 }:${process.env.DB_PORT}/${process.env.DB_NAME}${process.env.DB_QUERY}`;
